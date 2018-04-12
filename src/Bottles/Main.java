5fbeebb1a51f9ffce08eb0675bc6ed321ba9c8d5
chesseswar <chesseswar@gmail.com>
2018-04-12 15:59:42 -0500

    finished bottles! with bottom up dynamic programming!

--- src/Bottles/Main.java
+++ src/Bottles/Main.java
@@ -5,15 +5,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner("bottles.dat");
+        Scanner in = new Scanner(new File("bottles.dat"));
         int numCase = in.nextInt();
         while (numCase-- > 0){
             int bottles = in.nextInt();
-            String[] input = in.nextLine().split(" ");
+            String[] input = in.nextLine().trim().split(" ");
             int[] vals = new int[bottles];
             for (int i = 0; i < bottles; i++){
-                //vals[i] =
+                vals[i] = Integer.parseInt(input[i]);
             }
+
+            int[] maxVol = new int[vals.length];
+            maxVol[0] = vals[0];
+            maxVol[1] = Math.max(vals[0], vals[1]);
+            for (int i = 2; i < vals.length; i++){
+                int output = Math.max(maxVol[i-1], maxVol[i-2] + vals[i]);
+                maxVol[i] = output;
+            }
+            System.out.println(Arrays.toString(maxVol));
         }
     }
+
+    public static int maxVol(int[] bottles){
+        int[] memo = new int[bottles.length];
+        Arrays.fill(memo, -1);
+        memo[0] = bottles[0];
+        memo[1] = Math.max(bottles[0], bottles[1]);
+        return vol(bottles, bottles.length-1, memo);
+    }
+
+    public static int vol(int[] bottles, int index, int[] memo){
+        if (memo[index] != -1){
+            return memo[index];
+        } else {
+            memo[index] = Math.max(vol(bottles, index-2, memo) + bottles[index], vol(bottles, index-1, memo));
+            return memo[index];
+        }
+    }
+
+
 }
---
+++ src/Koch/Main.java
@@ -0,0 +1,12 @@
+package Koch;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] x = {0, 500, 1000};
+        int[] y = {1000, 0, 1000};
+
+    }
+}
