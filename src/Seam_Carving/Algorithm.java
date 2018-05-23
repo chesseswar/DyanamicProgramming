fa215c0eb20c03c4450325edfef17e796c6b36ff
chesseswar <chesseswar@gmail.com>
2018-05-23 16:32:05 -0500

    finished energy calc

--- src/Seam_Carving/Algorithm.java
+++ src/Seam_Carving/Algorithm.java
@@ -20,12 +20,12 @@ public class Algorithm {
 
         for (int i = 0; i < image.length; i++){
             for (int j = 0; j < image[0].length; j++){
-                image[i][j].energy = 1; //TODO: 1 IS NOT CORRECT, MAKE SURE YOU PUT ENERGY HERE INSTEAD
+                energy(j,i);
             }
         }
     }
 
-    public int energy(int x, int y){ //TODO: ENERGY METHOD NOT CORRECT, E = (DELTA COLOR)X^2 + (DELTA COLOR)Y^2
+    public void energy(int x, int y){
         int delRx = image[y][mod(x-1,w)].c.getRed();
         int delGx = image[y][mod(x-1,w)].c.getGreen();
         int delBx = image[y][mod(x-1,w)].c.getBlue();
@@ -33,9 +33,10 @@ public class Algorithm {
         int delRy = image[mod(y-1,h)][x].c.getRed();
         int delGy = image[mod(y-1,h)][x].c.getGreen();
         int delBy = image[mod(y-1,h)][x].c.getBlue();
-        image[y][x].energy = 0;
-        return x;
+        image[y][x].energy = delRx^2 + delGx^2 + delBx^2 + delRy^2 + delGy^2 + delBy^2;
     }
+
+    public int[] computeSeam
 
     public int mod(int a, int b){
         return (b + a%b)%b;
